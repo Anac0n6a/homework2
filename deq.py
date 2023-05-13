7f634641f62c80b74658b054b421af6120b03295
Tim Shmakov <119491011+Anac0n6a@users.noreply.github.com>
2023-05-14 00:08:07 +0200

    Update deq.py

--- deq.py
+++ deq.py
@@ -1,69 +1,77 @@
-# 87239549
+# 87261967
+
+class DequeError(Exception):
+    pass
+
 
 class Deque:
     def __init__(self, max_size):
-        self.buffer = [None] * max_size
-        self.front = 0
-        self.back = 0
-        self.size = 0
-        self.max_size = max_size
+        self.__buffer = [None] * max_size
+        self.__front = 0
+        self.__back = 0
+        self.__size = 0
+        self.__max_size = max_size
 
     def push_front(self, value):
-        if self.size == self.max_size:
-            print("error")
-        else:
-            self.front = (self.front - 1 + self.max_size) % self.max_size
-            self.buffer[self.front] = value
-            self.size += 1
+        if self.__size == self.__max_size:
+            raise DequeError("error")
+        self.__front = (self.__front - 1 +
+                        self.__max_size) % self.__max_size
+        self.__buffer[self.__front] = value
+        self.__size += 1
 
     def push_back(self, value):
-        if self.size == self.max_size:
-            print("error")
-        else:
-            self.buffer[self.back] = value
-            self.back = (self.back + 1) % self.max_size
-            self.size += 1
+        if self.__size == self.__max_size:
+            raise DequeError("error")
+        self.__buffer[self.__back] = value
+        self.__back = (self.__back + 1) % self.__max_size
+        self.__size += 1
 
     def pop_front(self):
-        if self.size == 0:
-            return "error"
-        else:
-            value = self.buffer[self.front]
-            self.buffer[self.front] = None
-            self.front = (self.front + 1) % self.max_size
-            self.size -= 1
-            return value
+        if self.__size == 0:
+            raise DequeError("error")
+        value = self.__buffer[self.__front]
+        self.__buffer[self.__front] = None
+        self.__front = (self.__front + 1) % self.__max_size
+        self.__size -= 1
+        return value
 
     def pop_back(self):
-        if self.size == 0:
-            return "error"
-        else:
-            self.back = (self.back - 1 + self.max_size) % self.max_size
-            value = self.buffer[self.back]
-            self.buffer[self.back] = None
-            self.size -= 1
-            return value
+        if self.__size == 0:
+            raise DequeError("error")
+        self.__back = (self.__back - 1 + self.__max_size) % self.__max_size
+        value = self.__buffer[self.__back]
+        self.__buffer[self.__back] = None
+        self.__size -= 1
+        return value
 
 
 team_number = int(input())
 max_deq_size = int(input())
 deque = Deque(max_deq_size)
 
-for i in range(team_number):
+for _ in range(team_number):
     command = input().split()
-    if command[0] == "push_front":
-        deque.push_front(int(command[1]))
-    elif command[0] == "push_back":
-        deque.push_back(int(command[1]))
-    elif command[0] == "pop_front":
-        value = deque.pop_front()
-        if value != "error":
-            print(value)
+    try:
+        method_name = command[0]
+        method = getattr(deque, method_name)
+        if len(command) > 1:
+            value = int(command[1])
+            if method_name == "push_front":
+                method(value)
+            elif method_name == "push_back":
+                method(value)
+            else:
+                raise AttributeError("Неверная команда")
         else:
-            print(value)
-    elif command[0] == "pop_back":
-        value = deque.pop_back()
-        if value != "error":
-            print(value)
-        else:
-            print(value)
+            value = method()
+            if value is not None:
+                print(value)
+            else:
+                raise DequeError(
+                    "Ошибка выполнения команды")
+    except AttributeError:
+        print("Неверная команда")
+    except DequeError as e:
+        print(e)
+
