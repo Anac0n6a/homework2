00de7ea45cc2f2559657a882ba112fa5ec66b33b
Tim Shmakov <119491011+Anac0n6a@users.noreply.github.com>
2023-05-14 15:39:28 +0200

    Update deq.py

--- deq.py
+++ deq.py
@@ -1,6 +1,9 @@
-# 87261967
+# 87284740
+class DequePushError(Exception):
+    pass
 
-class DequeError(Exception):
+
+class DequePopError(Exception):
     pass
 
 
@@ -14,7 +17,7 @@ class Deque:
 
     def push_front(self, value):
         if self.__size == self.__max_size:
-            raise DequeError("error")
+            raise DequePushError("error")
         self.__front = (self.__front - 1 +
                         self.__max_size) % self.__max_size
         self.__buffer[self.__front] = value
@@ -22,14 +25,14 @@ class Deque:
 
     def push_back(self, value):
         if self.__size == self.__max_size:
-            raise DequeError("error")
+            raise DequePushError("error")
         self.__buffer[self.__back] = value
         self.__back = (self.__back + 1) % self.__max_size
         self.__size += 1
 
     def pop_front(self):
         if self.__size == 0:
-            raise DequeError("error")
+            raise DequePopError("error")
         value = self.__buffer[self.__front]
         self.__buffer[self.__front] = None
         self.__front = (self.__front + 1) % self.__max_size
@@ -38,7 +41,7 @@ class Deque:
 
     def pop_back(self):
         if self.__size == 0:
-            raise DequeError("error")
+            raise DequePopError("error")
         self.__back = (self.__back - 1 + self.__max_size) % self.__max_size
         value = self.__buffer[self.__back]
         self.__buffer[self.__back] = None
@@ -46,32 +49,31 @@ class Deque:
         return value
 
 
-team_number = int(input())
-max_deq_size = int(input())
-deque = Deque(max_deq_size)
+if __name__ == '__main__':
+    team_number = int(input())
+    max_deq_size = int(input())
+    deq = Deque(max_deq_size)
 
-for _ in range(team_number):
-    command = input().split()
-    try:
-        method_name = command[0]
-        method = getattr(deque, method_name)
-        if len(command) > 1:
-            value = int(command[1])
-            if method_name == "push_front":
-                method(value)
-            elif method_name == "push_back":
-                method(value)
+    for _ in range(team_number):
+        command = input().split()
+        try:
+            method_name = command[0]
+            method = getattr(deq, method_name)
+            if len(command) > 1:
+                value = int(command[1])
+                if method_name == "push_front":
+                    method(value)
+                elif method_name == "push_back":
+                    method(value)
+                else:
+                    raise AttributeError("Неверная команда")
             else:
-                raise AttributeError("Неверная команда")
-        else:
-            value = method()
-            if value is not None:
-                print(value)
-            else:
-                raise DequeError(
-                    "Ошибка выполнения команды")
-    except AttributeError:
-        print("Неверная команда")
-    except DequeError as e:
-        print(e)
-
+                value = method()
+                if value is not None:
+                    print(value)
+                else:
+                    raise DequePopError("error")
+        except AttributeError:
+            print("Неверная команда")
+        except (DequePopError, DequePushError) as e:
+            print(e)
