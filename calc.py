9f44852eb074c1d390225d25b434ce490b43671d
Tim Shmakov <119491011+Anac0n6a@users.noreply.github.com>
2023-05-15 13:36:05 +0200

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,37 +1,37 @@
-# 87284129	
-# вообще все пришлось переделывать, все ломается и не проходит тесты
+# 87324227
 class CustomNoItemsException(Exception):
-    def __init__(self):
-        pass
+    pass
+
 
 class Stack:
     def __init__(self):
-        self.__data=[]
-        
-    def push(self,element):
+        self.__data = []
+
+    def push(self, element):
         self.__data.append(element)
-    
+
     def pop(self):
-        if len(self.__data)==0:
+        if len(self.__data) == 0:
             raise CustomNoItemsException
-        else:
-            return self.__data.pop()
+        return self.__data.pop()
+
 
 def calculate(input_string):
-    dictionary={
-    '+':lambda x,y:x+y,
-    '-':lambda x,y:y-x,
-    '*':lambda x,y:x*y,
-    '/':lambda x,y:y//x
+    operations = {
+        '+': lambda x, y: x+y,
+        '-': lambda x, y: y-x,
+        '*': lambda x, y: x*y,
+        '/': lambda x, y: y//x
     }
     operands = Stack()
-    for val in input_string.split(' '):
+    for val in input_string.split():
         try:
             operands.push(int(val))
         except ValueError:
-            operands.push(dictionary[val](operands.pop(),operands.pop()))
-    
+            operands.push(operations[val](operands.pop(), operands.pop()))
+
     return operands.pop()
+
 
 if __name__ == '__main__':
     print(calculate(input()))
